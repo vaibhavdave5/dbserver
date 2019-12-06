c24fead5a64a38b9d1d40bd1a4b9ab8ad4e4d931
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-12-06 16:16:04 -0500

    Changed the local to cloud mongo

--- data/db.js
+++ data/db.js
@@ -1,8 +1,8 @@
 module.exports = function () {
     const mongoose = require('mongoose');
     const databaseName = 'DB';
-    //var connectionString = 'mongodb+srv://vd1:vd1@cluster0-mismh.mongodb.net/'+databaseName+'?retryWrites=true';
-    var   connectionString = 'mongodb://localhost/';
+    var connectionString = 'mongodb+srv://vd1:vd1@cluster0-mismh.mongodb.net/'+databaseName+'?retryWrites=true';
+   // var   connectionString = 'mongodb://localhost/';
     connectionString += databaseName;
     mongoose.connect(connectionString);
 };
