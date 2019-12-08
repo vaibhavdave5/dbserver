51ca8ba057eaf48b56c05d706047d98953942f45
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-12-08 08:31:08 -0500

    Please work

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "*");
+        "https://radiant-forest-87925.herokuapp.com/");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
